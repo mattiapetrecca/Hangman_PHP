75096f50a837a1c64f566a686cee3889dec53c60
mattiapetrecca <160711559+mattiapetrecca@users.noreply.github.com>
2024-04-10 09:34:11 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,10 @@ session_start();
 $_SESSION['playerid'] = 0;
 $lista=file("parole.txt", FILE_IGNORE_NEW_LINES);
 $_SESSION["target"]=str_split($lista[mt_rand(0,count($lista)-1)]);
+$_SESSION["indovinate"]=[$_SESSION["target"][0]];
+for($i= 0;$i<count($_SESSION["target"]); $i++){
+    $_SESSION["indovinate"][$i]="_";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
