0ff21f92c9dcc38973a0e657060fb0ea5753800d
Vincentcolantonio <160608722+Vincentcolantonio@users.noreply.github.com>
2024-04-15 19:54:18 +0200

    Aggiunti round, "impiccagione" e render dinamico lettere

--- config.php
+++ config.php
@@ -1,26 +1,47 @@
 <?php 
 session_start();
-$_SESSION['playerid'] = 0;
-$_SESSION['round'] = 0;
+$_SESSION["fase"] = 0;
+//fix per far iniziare dal giocatore in posizione 0, dato che in load, la pagina aumenta di uno il playerid
+$_SESSION['playerid'] = -1;
+$_SESSION['round'] = 1;
+//Le lettere vengono poi messe come value di degli input HTML per far sì che il render delle stesse sia dinamico
+$_SESSION["lettere"] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 $lista=file("parole.txt", FILE_IGNORE_NEW_LINES);
 $_SESSION["target"]=str_split($lista[mt_rand(0,count($lista)-1)]);
 $_SESSION["indovinate"]=[$_SESSION["target"][0]];
-for($i= 0;$i<count($_SESSION["target"]); $i++){
+for($i = 1; $i < count($_SESSION["target"]); $i++){
     $_SESSION["indovinate"][$i]="_";
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>configurazioni</title>
+    <title>Configurazioni</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <form method="GET" action=game.php>
-        <input type="number" name="roundmax" required>
-        <input type="number" name="playercount" required>
-        <input type="submit" value="invia" required>
-</form>
+    <div class="container w-50">
+        <div class="row">
+            <div class="col gy-5">
+                <form method="GET" action=game.php>
+                    <div class="row">
+                        <div class="col">
+                            <input type="number" name="roundmax" placeholder="Numero round" min="1" max="5" required>
+                        </div>
+                        <div class="col">
+                            <input type="number" name="playercount" placeholder="Numero giocatori" min="2" max="4" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col gy-3">
+                            <input class="w-100 btn btn-primary" type="submit" value="INVIA">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- game.php
+++ game.php
@@ -1,4 +1,85 @@
-<?php session_start(); ?>
+<?php session_start(); 
+$presente = false;
+$lunghezza = count($_SESSION["target"]);
+$ultimaLettera = array_key_last($_SESSION["lettere"]);
+function reroll()  {
+    if ($_SESSION["round"] < $_SESSION["roundmax"]) {
+        //Si, fare un ciclo di immissione con gli ASCII sarebbe stato più veloce e semplice, ma deve essere comprensibile anche agli altri :)
+        $_SESSION["lettere"] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        $_SESSION["fase"] = 0;
+        $lista = file("parole.txt", FILE_IGNORE_NEW_LINES);
+        $_SESSION["target"] = str_split($lista[mt_rand(0, count($lista) - 1)]);
+        $lunghezza = count($_SESSION["target"]);
+        $_SESSION["indovinate"][0] = $_SESSION["target"][0];
+        for($i = 1; $i < $lunghezza; $i++){
+            $_SESSION["indovinate"][$i]="_";
+        }
+        $_SESSION["round"]++;
+        header("Refresh:0");
+    } else {
+        header("location: /risultati.php");
+    }
+}
+if (isset($_GET["playercount"])) {
+    $_SESSION["playercount"] = $_GET["playercount"];
+    for ($i = 0; $i < $_SESSION["playercount"]; $i++) {
+        $_SESSION["punti"][$i] = 0;
+    }
+    unset($_GET["playercount"]);
+}
+if (isset($_GET["roundmax"])) {
+    $_SESSION["roundmax"] = $_GET["roundmax"];
+    unset($_GET["roundmax"]);
+}
+//assegna i punti e fa reroll di una parola
+if (isset($_GET["guess"])) {
+    if ($_GET["guess"] != "") {
+        if (strtoupper($_GET["guess"]) === implode($_SESSION["target"])) {
+            $_SESSION["punti"][$_SESSION["playerid"]]++;
+            reroll();
+        } else {
+             $_SESSION["punti"][$_SESSION["playerid"]]--;
+             if ($_SESSION["fase"] === 5) {
+                reroll();
+             } else {
+                $_SESSION["fase"]++;
+             }
+        }
+        unset($_GET["letter"]);
+    }
+    unset($_GET["guess"]);
+}
+//controlla che sia stata scelta una lettera e se presente nella parola la inserisce tra le lettere visualizzate
+if (isset($_GET["letter"])) {
+    for ($i = 0; $i < $lunghezza; $i++){
+        if ($_GET["letter"] === $_SESSION["target"][$i]) {
+            $_SESSION["indovinate"][$i] = $_GET["letter"];
+            $presente = true;
+        }
+    }
+    //Se era l'unica lettera mancante per indovinare la parola, aggiunge un punto al giocatore che l'ha premuta e avanza alla prossima parola
+    if ($_SESSION["target"] === $_SESSION["indovinate"]) {
+        $_SESSION["punti"][$_SESSION["playerid"]]++;
+        reroll();
+    }
+    if (!$presente) {
+        if ($_SESSION["fase"] === 5) {
+            reroll();
+        } else {
+            $_SESSION["fase"]++;
+        }
+    }
+    //rimuove la lettere dall'array di lettere in maniera che al prossimo rerender esso non includa la lettera già provata
+    unset($_SESSION["lettere"][array_search($_GET["letter"], $_SESSION["lettere"])]);
+    unset($_GET["letter"]);
+}
+//aggiorna il turno basandosi su indici numerici che vanno da 0 al valore impostato su config.php
+if ($_SESSION["playerid"] < $_SESSION["playercount"] - 1) {
+    $_SESSION["playerid"]++;
+} else {
+    $_SESSION["playerid"] = 0;
+}
+?>
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -7,106 +88,57 @@
     <title>L'impiccato</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    <?php
-    if (isset($_GET["playercount"])) {
-        $_SESSION["playercount"] = $_GET["playercount"];
-        for ($i = 0; $i < $_SESSION["playercount"]; $i++) {
-            $_SESSION["punti"][$i] = 0;
-        }
-        unset($_GET["playercount"]);
-    }
-    if (isset($_GET["roundmax"])) {
-        $_SESSION["roundmax"] = $_GET["roundmax"];
-        unset($_GET["roundmax"]);
-    }
-    $i = 0;
-    $lunghezza = count($_SESSION["target"]);
-    //aggiorna il turno basandosi su indici numerici
-    if ($_SESSION["playerid"] < $_SESSION["playercount"] - 1) {
-        $_SESSION["playerid"]++;
-    } else {
-        $_SESSION["playerid"] = 0;
-    }
-    //assegna i punti e fa reroll di una parola
-    if (isset($_GET["guess"])) {
-        if ($_GET["guess"] != "") {
-            if (strtoupper($_GET["guess"]) === implode($_SESSION["target"])) {
-                $_SESSION["punti"][$_SESSION["playerid"]]++;
-                if ($_SESSION["round"] < $_SESSION["roundmax"]) {
-                    $lista = file("parole.txt", FILE_IGNORE_NEW_LINES);
-                    $_SESSION["target"] = str_split($lista[rand(0, count($lista) - 1)]);
-                    $_SESSION["indovinate"] = [$_SESSION["target"][0]];
-                    for ($i = 1; $i < $lunghezza; $i++) {
-                        $_SESSION["indovinate"][$i] = "_";
-                    }
-                }
-            } else {
-                 $_SESSION["punti"][$_SESSION["playerid"]]--;
-            }
-            unset($_GET["letter"]);
-        }
-        unset($_GET["guess"]);
-    }
-    //controlla che sia stata scelta una lettera e se presente nella parola la inserisce tra le lettere visualizzate
-    if (isset($_GET["letter"])) {
-        for ($i = 0; $i < $lunghezza; $i++){
-            if ($_GET["letter"] === $_SESSION["target"][$i]) {
-                $_SESSION["indovinate"][$i] = $_GET["letter"];
-            }
-        }
-        unset($_GET["letter"]);
-    }
-    ?>    
+<body class="text-center">
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col gy-5">
                 <h1><?php
-                //visualizza a schermo le lettere
+                //visualizza a schermo le lettere già indovinate o un trattino basso se non ancora
                 for ($i = 0; $i < $lunghezza; $i++) {
                     echo $_SESSION["indovinate"][$i]." ";
                 }
                 ?></h1>
             </div>
+            <div class="col gy-5">
+                <img src=<?php echo "\"img/impiccato".$_SESSION["fase"].".png\""?> alt=<?php echo "\"Errori: ".$_SESSION["fase"]." su 6\""?>>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h2>Tocca al giocatore <?php echo $_SESSION["playerid"]+1?></h2>
+            </div>
         </div>
         <div class="row">
             <div class="col">
                 <form action="#" method="GET">
-                    <input type="submit" name="letter" value="A">
-                    <input type="submit" name="letter" value="B">
-                    <input type="submit" name="letter" value="C">
-                    <input type="submit" name="letter" value="D">
-                    <input type="submit" name="letter" value="E">
-                    <input type="submit" name="letter" value="F">
-                    <input type="submit" name="letter" value="G">
-                    <input type="submit" name="letter" value="H">
-                    <input type="submit" name="letter" value="I">
-                    <input type="submit" name="letter" value="J">
-                    <input type="submit" name="letter" value="K">
-                    <input type="submit" name="letter" value="L">
-                    <input type="submit" name="letter" value="M">
-                    <input type="submit" name="letter" value="N">
-                    <input type="submit" name="letter" value="O">
-                    <input type="submit" name="letter" value="P">
-                    <input type="submit" name="letter" value="Q">
-                    <input type="submit" name="letter" value="R">
-                    <input type="submit" name="letter" value="S">
-                    <input type="submit" name="letter" value="T">
-                    <input type="submit" name="letter" value="U">
-                    <input type="submit" name="letter" value="V">
-                    <input type="submit" name="letter" value="W">
-                    <input type="submit" name="letter" value="X">
-                    <input type="submit" name="letter" value="Y">
-                    <input type="submit" name="letter" value="Z">
-                    <br/>
-                    <input type="text" name="guess" value="" placeholder="Inserire parola..."/>
-                    <input type="submit" value="INVIA"/>
+                    <div class="row">
+                        <div class="col gy-3">
+                            <?php 
+                            //Ciclo di render delle lettere, l'if serve a "tappare" i buchi nell'array dato che rimuovendo le lettere con unset l'array non si ridimensiona dinamicamente e shiftare tutti gli elementi per poi ridimensionarlo andrebbe a impattare sulle performance più di questo semplice if
+                            for ($i = 0; $i < $ultimaLettera + 1; $i++) {
+                                echo "<script>console.log(\"Indice: ".$i."\")</script>";
+                                if (isset($_SESSION["lettere"][$i])) {
+                                    echo "<input type=\"submit\" class=\"btn btn-secondary m-1\" name=\"letter\" value=\"".$_SESSION["lettere"][$i]."\">";
+                                }
+                            }
+                            ?>
+                            
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col gy-3">
+                            <input type="text" class="form-control" name="guess" value="" placeholder="Inserire parola..."/>
+                        </div>
+                        <div class="col gy-3">
+                            <input type="submit" class="form-control btn btn-primary" value="INVIA"/>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <table class="table text-center">
+                <table class="table">
                     <thead>
                         <tr>
                             <?php
@@ -128,7 +160,6 @@
                 </table>
             </div>
         </div>
-        
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
